5aa17762472b981f5ee641c9e23f7b97ec504c80
alpha0212 <qkrcjfgml321@naver.com>
2023-02-21 23:54:48 +0900

    ✨ Preview Title Component

---
+++ src/components/LandingComponents/Preview/Title/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import * as S from './styled';
+
+export const PreviewTitle: React.FC = () => {
+  return <S.PreviewTitleStyle>Preview</S.PreviewTitleStyle>;
+};
---
+++ src/components/LandingComponents/Preview/Title/styled.ts
@@ -0,0 +1,7 @@
+import styled from '@emotion/styled';
+
+export const PreviewTitleStyle = styled.div`
+  color: rgba(23, 30, 63, 1);
+  font-size: 30px;
+  font-weight: bold;
+`;
