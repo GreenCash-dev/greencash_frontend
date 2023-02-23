a18db24fa9e86cf1be674129a4565b3527eb7aa3
alpha0212 <qkrcjfgml321@naver.com>
2023-02-23 15:11:53 +0900

    ✨ Completed StepAbility Component

---
+++ src/components/LandingComponents/StepAbility/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import * as S from './styled';
+
+export const StepAbility: React.FC = () => {
+  return (
+    <S.StopAbilityDesc>
+      버리고자 하는 폐기물을 단계별로
+      <br />
+      가이드라인을 확인하며 순차적으로 분리배출을 하는 방식.
+      <br />
+      ‘한번에 인증’하는 방식보다는 진행이 느리지만
+      <br />
+      정확한 가이드라인을 확인하며 분리배출 가능함.
+    </S.StopAbilityDesc>
+  );
+};
