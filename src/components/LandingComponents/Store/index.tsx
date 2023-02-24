1964d456cf9e8f9f4b178dbc351bf62d38075373
alpha0212 <qkrcjfgml321@naver.com>
2023-02-25 00:42:41 +0900

    ✨ Store Desc Component

---
+++ src/components/LandingComponents/Store/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import * as S from './styled';
+
+export const Store: React.FC = () => (
+  <S.StoreDesc>
+    다양한 곳에서 캐시 사용가능해요.
+    <br />
+    특히 친환경 제품을 가장 많이 교환할 수 있습니다.
+  </S.StoreDesc>
+);
---
+++ src/components/LandingComponents/Store/styled.ts
@@ -0,0 +1,11 @@
+import styled from '@emotion/styled';
+
+export const StoreDesc = styled.div`
+  color: rgba(23, 30, 63, 0.5);
+
+  font-size: 15px;
+  font-weight: 500;
+  line-height: 29px;
+  text-align: right;
+  margin-top: 2rem;
+`;
