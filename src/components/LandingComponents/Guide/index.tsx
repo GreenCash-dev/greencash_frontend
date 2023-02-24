450c4105780998fa05f0ba50d7cae4edd1bc3803
alpha0212 <qkrcjfgml321@naver.com>
2023-02-25 00:44:14 +0900

    ✨

---
+++ src/components/LandingComponents/Guide/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import * as S from './styled';
+
+export const Guide: React.FC = () => (
+  <S.GuideDesc>
+    폐기물 각각의 분리배출 (배출) 방법을 담은 가이드라인.
+    <br />
+    가이드라인 내에서는 헷갈릴만한 품목을 <br />
+    눈에 띄게 보여줌.
+  </S.GuideDesc>
+);
---
+++ src/components/LandingComponents/Guide/styled.ts
@@ -0,0 +1,11 @@
+import styled from '@emotion/styled';
+
+export const GuideDesc = styled.div`
+  font-size: 15px;
+  font-weight: 500;
+  line-height: 29px;
+
+  color: rgba(23, 30, 63, 0.5);
+
+  margin-top: 2rem;
+`;
