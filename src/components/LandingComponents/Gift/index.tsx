b4318523d6191388b1fcc53946e30b822f1363f7
alpha0212 <qkrcjfgml321@naver.com>
2023-02-26 01:19:50 +0900

    ✨ Landing Gift Setion

---
+++ src/components/LandingComponents/Gift/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import * as S from './styled';
+
+export const Gift: React.FC = () => (
+  <S.GiftDesc>
+    스토어에서 물건을 구매하고 남은 캐시나
+    <br />
+    많은 분리배출로 얻게된 캐시를 환경을 위해
+    <br />
+    기부하는 공간.
+  </S.GiftDesc>
+);
---
+++ src/components/LandingComponents/Gift/styled.ts
@@ -0,0 +1,11 @@
+import styled from '@emotion/styled';
+
+export const GiftDesc = styled.div`
+  font-size: 15px;
+  font-weight: 500;
+
+  line-height: 29px;
+  color: rgba(23, 30, 63, 0.5);
+
+  margin-top: 2rem;
+`;
