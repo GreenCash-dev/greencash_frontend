fb6b73767569fa81adc504bd2acbe15a27805d65
alpha0212 <qkrcjfgml321@naver.com>
2023-02-27 23:44:07 +0900

    ✨ position sticky Navbar Component

---
+++ src/components/common/Navbar/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import * as S from './styled';
+
+export const Navbar: React.FC = () => {
+  return (
+    <S.NavbarBox>
+      <S.NavbarText>Greencash</S.NavbarText>
+    </S.NavbarBox>
+  );
+};
---
+++ src/components/common/Navbar/styled.ts
@@ -0,0 +1,21 @@
+import styled from '@emotion/styled';
+
+export const NavbarBox = styled.div`
+  top: 0;
+  width: 100%;
+  height: 70px;
+  display: flex;
+  position: sticky;
+
+  background-color: #ffffff;
+  box-shadow: rgb(0 0 0 / 4%) 0px 7px 10px 0px;
+`;
+
+export const NavbarText = styled.div`
+  margin: auto;
+  font-size: 22px;
+  font-weight: bold;
+  color: rgba(173, 214, 5, 1);
+
+  letter-spacing: 1px;
+`;
