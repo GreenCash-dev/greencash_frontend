d6407dd846e470b94950c53600698adbf4385366
alpha0212 <qkrcjfgml321@naver.com>
2023-02-27 23:11:37 +0900

    👔 Completed styling CashOnHand Component

---
+++ src/components/MainComponents/CashOnHand/styled.ts
@@ -0,0 +1,41 @@
+import styled from '@emotion/styled';
+
+export const CashOnHandBox = styled.div`
+  width: 77px;
+  height: 23px;
+
+  border: 2px solid rgba(185, 222, 42, 1);
+  border-radius: 20px;
+
+  display: flex;
+  background-color: rgba(252, 253, 253, 1);
+`;
+export const CashOnHandIcon = styled.div`
+  width: 12px;
+  height: 12px;
+
+  border: 2px solid rgba(185, 222, 42, 1);
+  border-radius: 10px;
+
+  margin: auto;
+  margin-left: 10px;
+  margin-right: 0;
+`;
+export const CashOnHandIconText = styled.div`
+  font-size: 13px;
+  text-align: center;
+  color: rgba(185, 222, 42, 1);
+
+  margin-top: -2.5px;
+  margin-right: 1px;
+`;
+
+export const CashOnHandValue = styled.div`
+  font-size: 15px;
+  font-weight: bold;
+  color: rgba(185, 222, 42, 1);
+
+  margin: auto;
+  margin-left: 4px;
+  margin-top: 3px;
+`;
