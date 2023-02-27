5fd316433b7fcc5686988fc6b1097ac35d1f1f65
alpha0212 <qkrcjfgml321@naver.com>
2023-02-27 23:47:47 +0900

    🔨 App Version

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,39 @@
 import React from 'react';
 import { Routes, Route } from 'react-router';
-import { Landing } from './pages';
+
+import { Navbar } from './components';
+import styled from '@emotion/styled';
+import { MainPage } from './pages';
 
 const App: React.FC = () => {
   return (
-    <Routes>
-      <Route index element={<Landing />} />
-    </Routes>
+    <MediaResponsive>
+      <Navbar />
+      <MainScreen>
+        <Routes>
+          <Route index element={<MainPage />} />
+        </Routes>
+      </MainScreen>
+    </MediaResponsive>
   );
 };
+
+export const MediaResponsive = styled.div`
+  @media screen and (max-width: 768px), screen and (min-width: 768px) {
+    width: 425px;
+    height: 100%;
+  }
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  margin: 0px auto;
+  background-color: rgba(245, 245, 245, 1);
+`;
+
+export const MainScreen = styled.main`
+  position: relative;
+  width: 370px;
+  margin: auto;
+`;
 
 export default App;
