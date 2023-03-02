9f7bf074030b1fcff893cc955da950c73bc2d4df
alpha0212 <qkrcjfgml321@naver.com>
2023-03-03 00:08:33 +0900

    ✨ Campaign -> LandingCampaign 메인 앱이랑 함수이름이 같아서 변경

---
+++ src/components/LandingComponents/LandingCampaign/styled.ts
@@ -0,0 +1,9 @@
+import styled from '@emotion/styled';
+
+export const CampaignDesc = styled.div`
+  color: rgba(23, 30, 63, 0.5);
+  font-size: 15px;
+  font-weight: 500;
+  line-height: 29px;
+  margin-top: 2rem;
+`;
