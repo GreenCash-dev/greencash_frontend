0916e2e714993d958d6c8ee6e0b4e34e9beea463
alpha0212 <qkrcjfgml321@naver.com>
2023-03-03 10:47:09 +0900

    📁 File Routing

--- src/components/MainComponents/index.ts
+++ src/components/MainComponents/index.ts
@@ -2,6 +2,8 @@ export * from './CashOnHand';
 export * from './Campaign';
 export * from './GuideLine';
 export * from './Give';
+export * from './Question';
 export * from './Store';
 export * from './StepCertification';
 export * from './OnceCertification';
+//abcdefghijklmnopqrstuvwxyz
