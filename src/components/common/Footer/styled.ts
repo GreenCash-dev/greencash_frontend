fe65c9c6bf63cb20ca3907607be72413a72e4390
alpha0212 <qkrcjfgml321@naver.com>
2023-03-03 10:43:46 +0900

    ✨ Add Footer

---
+++ src/components/common/Footer/styled.ts
@@ -0,0 +1,12 @@
+import styled from '@emotion/styled';
+
+export const FooterBox = styled.footer`
+  bottom: 0;
+  width: 100%;
+  height: 70px;
+  display: flex;
+  position: sticky;
+
+  background-color: #000000;
+  z-index: 99;
+`;
