9a40bc61b29080391ba456ec068402e41c081756
alpha0212 <qkrcjfgml321@naver.com>
2023-03-03 10:44:32 +0900

    ✨ Completed Question Component

---
+++ src/components/MainComponents/Question/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import * as S from './styled';
+import QuestionIcon from '@assets/Question.svg';
+
+export const Question: React.FC = () => {
+  return (
+    <S.QuestionBox>
+      <S.QuestionPosition>
+        <S.QuestionIcon src={QuestionIcon} />
+        <S.QuestionText>자주 물어보는 질문</S.QuestionText>
+      </S.QuestionPosition>
+    </S.QuestionBox>
+  );
+};
---
+++ src/components/MainComponents/Question/styled.ts
@@ -0,0 +1,25 @@
+import styled from '@emotion/styled';
+
+export const QuestionBox = styled.div`
+  width: 100%;
+  height: 65px;
+  display: flex;
+
+  border-radius: 10px;
+  box-shadow: rgb(0 0 0 / 4%) 0px 7px 10px 0px;
+  background-color: #ffffff;
+`;
+
+export const QuestionPosition = styled.div`
+  display: flex;
+  margin: auto auto auto 18px;
+`;
+export const QuestionIcon = styled.img`
+  height: 20px;
+`;
+export const QuestionText = styled.div`
+  color: rgba(0, 0, 0, 1);
+  font-size: 18px;
+  font-weight: bold;
+  margin-left: 15px;
+`;
