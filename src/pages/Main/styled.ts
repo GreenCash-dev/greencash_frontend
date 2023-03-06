64f08652ad6696d928b360cb3d6a59809c00a81b
alpha0212 <qkrcjfgml321@naver.com>
2023-03-06 23:44:40 +0900

    🔨 Footer 메인에 추가

--- src/pages/Main/styled.ts
+++ src/pages/Main/styled.ts
@@ -47,3 +47,42 @@ export const DoGiveContainer = styled.div`
 export const QAContainer = styled.section`
   width: 100%;
 `;
+//Footer
+export const FooterBox = styled.footer`
+  bottom: 0;
+  width: 100%;
+  height: 70px;
+  display: flex;
+  position: sticky;
+
+  background-color: #ffffff;
+  z-index: 99;
+`;
+
+export const FooterContainer = styled.div`
+  width: 80%;
+  margin: auto;
+  display: flex;
+  & > * {
+    //자식 스타일
+    cursor: pointer;
+    margin: auto;
+  }
+`;
+
+export const FooterMainIconContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+export const FooterMissionIconContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+export const FooterSearchIconContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+export const FooterProfileIconContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
