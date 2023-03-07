909368691521af249e261e4f0f644c2604e2aba5
alpha0212 <qkrcjfgml321@naver.com>
2023-03-08 00:00:27 +0900

    ✨ Add Input Component in DoGive

---
+++ src/components/DoGiveComponents/Input/styled.ts
@@ -0,0 +1,23 @@
+import styled from '@emotion/styled';
+
+export const DoGiveInputContainer = styled.div`
+  width: 100%;
+  height: 44px;
+`;
+export const DoGiveInputElement = styled.input`
+  width: 100%;
+  height: 100%;
+
+  font-size: 15px;
+  font-weight: bold;
+  text-align: center;
+  color: #616368;
+  border: 1.5px solid #d6d7d7;
+  border-radius: 10px;
+
+  outline: none;
+  ::placeholder {
+    color: #b3b8c8;
+  }
+  background-color: #f1f2f2;
+`;
