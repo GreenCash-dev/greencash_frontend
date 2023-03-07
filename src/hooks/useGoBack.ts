7b4f24a5cc8ff97ec4c553fbd87e9cfcbbc44205
alpha0212 <qkrcjfgml321@naver.com>
2023-03-08 00:01:57 +0900

    🔨 제작중: 뒤로가기

---
+++ src/hooks/useGoBack.ts
@@ -0,0 +1,7 @@
+import { useCallback } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+export const useGoBack = () => {
+  const navigate = useNavigate();
+  //   return useCallback(navigate(-1), []);
+};
