d00fcd07a0acb7e29bf631bf123bdeda223cdb62
alpha0212 <qkrcjfgml321@naver.com>
2023-03-08 15:40:19 +0900

    ✨ GoBack Icon

--- src/components/common/Goback/index.tsx
+++ src/components/common/Goback/index.tsx
@@ -3,5 +3,13 @@ import * as S from './styled';
 import { useGoBack as GoBack } from '@src/hooks';
 
 import LeftArrowIcon from '@assets/LeftArrow.svg';
+import { NavigateFunction } from 'react-router';
 
-export const GobackIcon: React.FC = () => <S.LeftArrow onClick={GoBack} src={LeftArrowIcon} />;
+interface PathMoveProps {
+  MoveWhere: 1 | -1;
+  NavigateFunc: NavigateFunction;
+}
+
+export const GobackIcon: React.FC<PathMoveProps> = ({ MoveWhere, NavigateFunc }) => (
+  <S.LeftArrow onClick={() => NavigateFunc(MoveWhere)} src={LeftArrowIcon} />
+);
