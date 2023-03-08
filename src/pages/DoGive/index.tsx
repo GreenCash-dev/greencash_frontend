2e8771cbb59c63ace91c9f6a50c2e7c764eff68b
alpha0212 <qkrcjfgml321@naver.com>
2023-03-08 15:41:37 +0900

    ✨ Modify DoGivePage

--- src/pages/DoGive/index.tsx
+++ src/pages/DoGive/index.tsx
@@ -1,26 +1,49 @@
-import { DoGiveDesc, DoGiveInput, GobackIcon } from '@src/components';
+import { Button, DoGiveDesc, DoGiveInput, DoingGive, GobackIcon } from '@src/components';
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import * as S from './styled';
+import PlantIcon from '@assets/Plant.svg';
+import UpArrowIcon from '@assets/UpArrow.svg';
+import { useEffect } from 'react';
 
 export const DoGivePage: React.FC = () => {
-  const [haveCash, setHaveCash] = useState(0); //보유중인 캐시
+  const [haveCash, setHaveCash] = useState(1000); //보유중인 캐시
   const [doingGive, setDoingGive] = useState(0); //사람들이 기부중인 캐시
+  const [giveCash, setGiveCash] = useState(''); //
+  const [as, setAs] = useState(0);
   const navigate = useNavigate();
+  // eslint-disable-next-line prefer-const
+  const state = Number(giveCash);
+  console.log(state);
   return (
     <S.DoGivePageContainer>
       <S.GoBackSection>
-        <GobackIcon />
+        <GobackIcon MoveWhere={-1} NavigateFunc={navigate} />
       </S.GoBackSection>
       <S.DoGiveDescSection>
         <DoGiveDesc />
       </S.DoGiveDescSection>
       <S.DoGiveCashInputSection>
-        <DoGiveInput />
+        <DoGiveInput setGiveCash={setGiveCash} />
+        {haveCash - state < 0 && <S.WarningText>캐시가 부족합니다.</S.WarningText>}
       </S.DoGiveCashInputSection>
-      <S.DoGiveCashInfoSection></S.DoGiveCashInfoSection>
-      <S.DoGiveButtonSection></S.DoGiveButtonSection>
+      <S.DoGiveCashInfoSection>
+        <DoingGive GivingCash={doingGive} PlantIconSrc={PlantIcon} OwnCash={haveCash} UpArrowIconSrc={UpArrowIcon} />
+      </S.DoGiveCashInfoSection>
+      <S.DoGiveButtonSection>
+        <Button
+          CashIsMinus={haveCash - state < 0}
+          OnClick={() => {
+            setDoingGive(Number(doingGive + state));
+            setHaveCash(haveCash - state);
+          }}
+          BackgroundColor="#B5E565"
+          fontColor="#ffffff"
+          fontSize="16px"
+          DoText="기부하기"
+        />
+      </S.DoGiveButtonSection>
     </S.DoGivePageContainer>
   );
 };
--- src/pages/DoGive/styled.ts
+++ src/pages/DoGive/styled.ts
@@ -1,15 +1,37 @@
 import styled from '@emotion/styled';
 
 export const DoGivePageContainer = styled.section`
-  width: 80%;
-`;
-
-export const GoBackSection = styled.section``;
-export const DoGiveDescSection = styled.section`
+  width: 85%;
+  height: 100vh;
+  margin: auto;
   display: flex;
   flex-direction: column;
 `;
-export const DoGiveCashInputSection = styled.section``;
+
+export const GoBackSection = styled.section`
+  margin: 33px auto 23px 0;
+`;
+export const DoGiveDescSection = styled.section`
+  display: flex;
+  flex-direction: column;
+  & > * {
+    margin-bottom: 10px;
+  }
+  margin-top: 10px;
+`;
+export const DoGiveCashInputSection = styled.section`
+  margin-top: 13px;
+  margin-bottom: 23px;
+`;
 export const DoGiveCashInfoSection = styled.section``;
 
-export const DoGiveButtonSection = styled.section``;
+export const DoGiveButtonSection = styled.section`
+  width: 100%;
+  margin: auto auto 40px auto;
+`;
+
+export const WarningText = styled.span`
+  color: #ef4444;
+  font-size: 12px;
+  text-align: left;
+`;
