67fed0817f3919194d85d6f9bf15c0b8afa607c8
alpha0212 <qkrcjfgml321@naver.com>
2023-03-12 23:39:42 +0900

    ✨ 카메라 페이지

--- src/pages/OnceCert/index.tsx
+++ src/pages/OnceCert/index.tsx
@@ -1,5 +1,60 @@
-import React from 'react';
+import { GobackIcon } from '@src/components';
+import axios from 'axios';
+import React, { useRef, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Webcam from 'react-webcam';
 
-export const OnceCertificationPage: React.FC = () => {
-  return <></>;
+import * as S from './styled';
+
+const videoConstraints = {
+  width: 375,
+  height: 600,
+  facingMode: 'environment',
+};
+
+export const OnceCertCapturePage: React.FC = () => {
+  const [imgSrc, setImgSrc] = useState<string>('');
+  const [send, setSend] = useState<boolean>(false);
+  const cameraRef = useRef<Webcam>(null);
+  const navigate = useNavigate();
+  const capture = React.useCallback(() => {
+    const imageSrc = cameraRef.current?.getScreenshot();
+    if (imageSrc) {
+      sendData(imageSrc);
+      setImgSrc(imageSrc);
+    }
+  }, [cameraRef]);
+  const sendData = (c: any) => {
+    console.log(c);
+    const headers = {
+      accept: 'application/json',
+    };
+
+    const fd = new FormData();
+    fd.append('image', c);
+
+    axios
+      .post('url', fd, { headers: headers })
+      .then((res) => {
+        console.log(res.data);
+        setSend(true);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  return (
+    <S.OnceCertScreen>
+      <S.GoBackContainer>
+        <GobackIcon MoveWhere={-1} NavigateFunc={navigate} />
+      </S.GoBackContainer>
+      <S.CameraScreen>as</S.CameraScreen>
+      <S.CameraOptions>
+        <S.CaptureButton>
+          <S.BlackCircle>
+            <S.WhiteCenterCircle />
+          </S.BlackCircle>
+        </S.CaptureButton>
+      </S.CameraOptions>
+    </S.OnceCertScreen>
+  );
 };
