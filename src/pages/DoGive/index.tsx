010017cd3aae33d587c972de4b8ea427ce666357
alpha0212 <qkrcjfgml321@naver.com>
2023-03-13 22:56:48 +0900

    ✨ 기부중인 금액 전역적으로 관리 -> 필요X -> 리팩토링해야함

--- src/pages/DoGive/index.tsx
+++ src/pages/DoGive/index.tsx
@@ -6,15 +6,18 @@ import * as S from './styled';
 import PlantIcon from '@assets/Plant.svg';
 import UpArrowIcon from '@assets/UpArrow.svg';
 import { useRecoilState, useSetRecoilState } from 'recoil';
-import { giveModalState } from '@src/atom/GiveSuccessAtom';
+import { DonatingCashState, SuccessModalState, havingCashState } from '@src/atom';
 import { GiveSuccessModal } from '@src/components/Modal';
 
 export const DoGivePage: React.FC = () => {
-  const [haveCash, setHaveCash] = useState(1000); //보유중인 캐시
-  const [doingGive, setDoingGive] = useState(0); //사람들이 기부중인 캐시
+  const [havingCash, setHavingCash] = useRecoilState(havingCashState); // 보유중인 캐시
+  const setHavingCashState = useSetRecoilState(havingCashState); //값 설정
+
+  const [donatingCash, setDonatingCash] = useRecoilState(DonatingCashState);
+  const setDonatingCashState = useSetRecoilState(DonatingCashState);
   const [giveCash, setGiveCash] = useState(''); //
-  const setGiveModalState = useSetRecoilState(giveModalState);
-  const [modalState, setModalState] = useRecoilState(giveModalState);
+  const setGiveModalState = useSetRecoilState(SuccessModalState);
+  const [modalState, setModalState] = useRecoilState(SuccessModalState);
   const handleClose = () => {
     setModalState((prev) => ({
       ...prev,
@@ -28,24 +31,35 @@ export const DoGivePage: React.FC = () => {
   return (
     <S.DoGivePageContainer>
       <S.GoBackSection>
-        <GobackIcon MoveWhere={-1} NavigateFunc={navigate} />
+        <GobackIcon MoveWhere={'/'} NavigateFunc={navigate} />
       </S.GoBackSection>
       <S.DoGiveDescSection>
         <DoGiveDesc />
       </S.DoGiveDescSection>
       <S.DoGiveCashInputSection>
         <DoGiveInput setGiveCash={setGiveCash} />
-        {haveCash - state < 0 && <S.WarningText>캐시가 부족해요.</S.WarningText>}
+        {havingCash.cash - state < 0 && <S.WarningText>캐시가 부족해요.</S.WarningText>}
       </S.DoGiveCashInputSection>
       <S.DoGiveCashInfoSection>
-        <DoingGive GivingCash={doingGive} PlantIconSrc={PlantIcon} OwnCash={haveCash} UpArrowIconSrc={UpArrowIcon} />
+        <DoingGive
+          GivingCash={donatingCash.bedonated}
+          PlantIconSrc={PlantIcon}
+          OwnCash={havingCash.cash}
+          UpArrowIconSrc={UpArrowIcon}
+        />
       </S.DoGiveCashInfoSection>
       <S.DoGiveButtonSection>
         <Button
-          CashIsMinus={haveCash - state < 0 || state < 0 || !state}
+          CashIsMinus={havingCash.cash - state < 0 || state < 0 || !state}
           OnClick={() => {
-            setDoingGive(Number(doingGive + state));
-            setHaveCash(haveCash - state);
+            setDonatingCashState((prev) => ({
+              ...prev,
+              bedonated: prev.bedonated + state,
+            }));
+            setHavingCashState((prev) => ({
+              ...prev,
+              cash: prev.cash - state,
+            }));
             setGiveModalState((prev) => ({
               ...prev,
               view: 'success',
