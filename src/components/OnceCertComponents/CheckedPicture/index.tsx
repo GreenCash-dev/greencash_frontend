a509d8932e6d4608e1ed79878d15be0f2529afe2
alpha0212 <qkrcjfgml321@naver.com>
2023-03-13 19:36:26 +0900

    ✨ Checked Icon

---
+++ src/components/OnceCertComponents/CheckedPicture/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from '@emotion/styled';
+
+export const CheckedPicture: React.FC = () => (
+  <CheckedIcon src={'https://cdn-icons-png.flaticon.com/128/5709/5709772.png'} />
+);
+
+export const CheckedIcon = styled.img`
+  width: 20px;
+  height: 20px;
+  margin: auto;
+`;
