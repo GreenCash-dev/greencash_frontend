35e5dd89e04b0f5fca2b406f78e4e554814da50e
alpha0212 <qkrcjfgml321@naver.com>
2023-03-13 20:41:46 +0900

    🔨 스타일을 정할 props를 받아낸다

--- src/components/OnceCertComponents/CaptureOptions/styled.ts
+++ src/components/OnceCertComponents/CaptureOptions/styled.ts
@@ -1,20 +1,31 @@
 import styled from '@emotion/styled';
 
-export const OptionsBox = styled.div`
+export const OptionsContainer = styled.div<{ NowCertification: boolean }>`
+  display: flex;
+  flex-direction: column;
+
+  margin: auto;
+  opacity: ${(props) => (props.NowCertification ? 0.3 : 1)};
+`;
+export const OptionsButton = styled.button`
   width: 50px;
   height: 50px;
 
-  cursor: pointer;
   display: flex;
+  border: none;
   border-radius: 7px;
+
   box-shadow: rgba(0, 0, 0, 0.15) 0px 5px 15px 0px;
   background-color: #ffffff;
+  cursor: pointer;
 `;
 
-export const OptionsIcon = styled.img`
+export const OptionsIcon = styled.img<{ NoneReverse: boolean }>`
   width: 20px;
   height: 20px;
   margin: auto;
+
+  rotate: ${(props) => (props.NoneReverse ? '0deg' : '180deg')};
 `;
 export const OptionsDesc = styled.div`
   margin-top: 10px;
@@ -22,10 +33,4 @@ export const OptionsDesc = styled.div`
   color: #000000;
   text-align: center;
   font-size: 12px;
-`;
-export const OptionsContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-
-  margin: auto;
 `;
