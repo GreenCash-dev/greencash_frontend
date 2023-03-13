bd7c9eb7087084c71d298c7aab9a359bf4c0360f
alpha0212 <qkrcjfgml321@naver.com>
2023-03-13 22:55:17 +0900

    🔨 Change Type of MoveWhere

--- src/components/common/Goback/index.tsx
+++ src/components/common/Goback/index.tsx
@@ -5,10 +5,10 @@ import LeftArrowIcon from '@assets/LeftArrow.svg';
 import { NavigateFunction } from 'react-router';
 
 interface PathMoveProps {
-  MoveWhere: 1 | -1;
+  MoveWhere: '/' | -1;
   NavigateFunc: NavigateFunction;
 }
 
 export const GobackIcon: React.FC<PathMoveProps> = ({ MoveWhere, NavigateFunc }) => (
-  <S.LeftArrow alt="뒤로가기" onClick={() => NavigateFunc(MoveWhere)} src={LeftArrowIcon} />
+  <S.LeftArrow alt="뒤로가기" onClick={() => NavigateFunc(MoveWhere as string)} src={LeftArrowIcon} />
 );
