00a2395190f457895b6dd50ca95f2596a7a2c376
alpha0212 <qkrcjfgml321@naver.com>
2023-03-13 10:53:48 +0900

    📁 OnceCertComponent folder Routing

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,4 +3,5 @@ export * from './MainComponents';
 export * from './common';
 export * from './FooterComponents';
 export * from './DoGiveComponents';
+export * from './OnceCertComponents';
 export * from './Modal';
