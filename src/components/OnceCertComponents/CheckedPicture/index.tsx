bec19bd9336a79382a76f1d9eb2b513e1a0f480a
alpha0212 <qkrcjfgml321@naver.com>
2023-03-13 20:42:30 +0900

    ✨ 사진이 있다면 체크 아니라면 Circle Icon

--- src/components/OnceCertComponents/CheckedPicture/index.tsx
+++ src/components/OnceCertComponents/CheckedPicture/index.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
 import styled from '@emotion/styled';
 
-export const CheckedPicture: React.FC = () => (
-  <CheckedIcon src={'https://cdn-icons-png.flaticon.com/128/5709/5709772.png'} />
-);
+interface CheckedQProps {
+  NoneChecked: boolean;
+}
+
+export const CheckedPicture: React.FC<CheckedQProps> = ({ NoneChecked }) =>
+  NoneChecked ? <NoneChekcedIcon /> : <CheckedIcon src={'https://cdn-icons-png.flaticon.com/128/5709/5709772.png'} />;
 
 export const CheckedIcon = styled.img`
-  width: 20px;
-  height: 20px;
-  margin: auto;
+  width: 17px;
+  height: 17px;
+
+  margin: 0 5px 0 5px;
+`;
+
+export const NoneChekcedIcon = styled.div`
+  width: 13px;
+  height: 13px;
+
+  margin: 0 5px 0 5px;
+  border: 1.5px solid #000000;
+  border-radius: 20px;
 `;
