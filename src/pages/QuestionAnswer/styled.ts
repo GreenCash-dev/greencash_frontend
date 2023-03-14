3faf737993e37bc22f84062971bfb9dd74ee13ae
alpha0212 <qkrcjfgml321@naver.com>
2023-03-15 00:38:40 +0900

    design(pages): Fill the screen height

--- src/pages/QuestionAnswer/styled.ts
+++ src/pages/QuestionAnswer/styled.ts
@@ -1,1 +1,13 @@
 import styled from '@emotion/styled';
+
+export const QuestionAnswerContainer = styled.main`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const GoBackContainer = styled.section`
+  display: flex;
+  margin-top: 40px;
+`;
