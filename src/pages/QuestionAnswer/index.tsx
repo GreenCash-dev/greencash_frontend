6485180db0c710b410c96176478acdec7a02c11d
alpha0212 <qkrcjfgml321@naver.com>
2023-03-15 00:37:48 +0900

    feat(pages): add gobackIcon in QuestionAnswer

--- src/pages/QuestionAnswer/index.tsx
+++ src/pages/QuestionAnswer/index.tsx
@@ -1,7 +1,16 @@
+import { GobackIcon } from '@src/components';
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import * as S from './styled';
 
 export const QuestionAndAnswerPage: React.FC = () => {
-  return <></>;
+  const navigate = useNavigate();
+  return (
+    <S.QuestionAnswerContainer>
+      <S.GoBackContainer>
+        <GobackIcon MoveWhere={-1} NavigateFunc={navigate} />
+      </S.GoBackContainer>
+    </S.QuestionAnswerContainer>
+  );
 };
