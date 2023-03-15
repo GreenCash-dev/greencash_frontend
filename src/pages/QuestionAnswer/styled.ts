9ce43724ad489d537cf736e210b3140522f1cbf0
alpha0212 <qkrcjfgml321@naver.com>
2023-03-15 21:17:25 +0900

    feat(QuestionAndAnswerPage): Modified the container width.

--- src/pages/QuestionAnswer/styled.ts
+++ src/pages/QuestionAnswer/styled.ts
@@ -1,8 +1,10 @@
 import styled from '@emotion/styled';
 
-export const QuestionAnswerContainer = styled.main`
-  width: 100%;
+export const QuestionAnswerContainer = styled.div`
+  width: 90%;
   height: 100vh;
+
+  margin: auto;
   display: flex;
   flex-direction: column;
 `;
@@ -10,4 +12,29 @@ export const QuestionAnswerContainer = styled.main`
 export const GoBackContainer = styled.section`
   display: flex;
   margin-top: 40px;
+`;
+
+//#########faq
+
+export const QnATitle = styled.div`
+  color: #000000;
+  font-size: 18px;
+  font-weight: bold;
+  margin-top: -3px;
+  margin-left: 20px;
+`;
+export const QnAElement = styled.div`
+  display: flex;
+  flex-direction: column;
+  & > * {
+    cursor: pointer;
+  }
+`;
+
+export const QnAContainer = styled.section`
+  width: 100%;
+  margin: 50px auto auto auto;
+
+  display: flex;
+  flex-direction: column;
 `;
