5b8be774510e93f688539503d021083c65f07ae6
alpha0212 <qkrcjfgml321@naver.com>
2023-03-15 20:57:55 +0900

    fix(QuestionComponent): Go to path qna when user click question

--- src/components/MainComponents/Question/index.tsx
+++ src/components/MainComponents/Question/index.tsx
@@ -5,7 +5,7 @@ import QuestionIcon from '@assets/Question.svg';
 
 export const Question: React.FC = () => {
   return (
-    <S.QuestionBox>
+    <S.QuestionBox to="qna">
       <S.QuestionPosition>
         <S.QuestionIcon src={QuestionIcon} />
         <S.QuestionText>자주 물어보는 질문</S.QuestionText>
