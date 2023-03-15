9b60db4016646a9f0766acbd36bff164cc609e10
alpha0212 <qkrcjfgml321@naver.com>
2023-03-15 21:25:32 +0900

    feat(components/Fqa/QnABox: An array of qna was received as props and applied to the QnABoxComponent.

---
+++ src/components/FqaComponents/QnABox/index.tsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import styled from '@emotion/styled';
+
+interface ClickFuncProps {
+  faqArray: { question: string; answer: string; open: boolean };
+}
+
+export const QnABox: React.FC<ClickFuncProps> = ({ faqArray }) => {
+  return (
+    <>
+      <QuestionElement clickthis={faqArray.open}>
+        <QuestionText clickthis={faqArray.open}>{faqArray.question}</QuestionText>
+      </QuestionElement>
+      <AnswerElement clickthis={faqArray.open}>
+        <AnswerElementText clickthis={faqArray.open}>
+          {faqArray.answer.slice(0, 25)} <br />
+          {faqArray.answer.slice(25, -1)}
+        </AnswerElementText>
+      </AnswerElement>
+    </>
+  );
+};
+
+//qna
+
+export const QuestionElement = styled.button<{ clickthis: boolean }>`
+  width: 315px;
+  height: 55px;
+  :after {
+    height: 110px;
+  }
+
+  display: flex;
+  flex-direction: column;
+  margin: 25px auto 20px auto;
+
+  transition: all 0.2s ease 0s;
+  border: none;
+  border-radius: ${(props) => (props.clickthis ? '10px 10px 0 0' : '10px')};
+  background-color: ${(props) => (props.clickthis ? '#0f131e' : '#ffffff')};
+  box-shadow: rgba(0, 0, 0, 0.05) 0px 2px 10px 2px;
+`;
+
+export const QuestionText = styled.div<{ clickthis: boolean }>`
+  color: ${(props) => (props.clickthis ? '#ffffff' : '#535353')};
+
+  margin: auto;
+  text-align: center;
+  font-size: 15px;
+  font-weight: bold;
+`;
+
+export const AnswerElement = styled.div<{ clickthis: boolean }>`
+  width: 315px;
+  height: ${(props) => (props.clickthis ? '60px' : '0')};
+  display: flex;
+
+  transition: 0.2s ease-out 0s;
+  margin: -20px auto 0 auto;
+  border-radius: 0 0 10px 10px;
+  background-color: #0f131e;
+`;
+
+export const AnswerElementText = styled.div<{ clickthis: boolean }>`
+  ${(props) => (props.clickthis ? '' : 'display: none')};
+  color: ${(props) => (props.clickthis ? '#ffffff' : '0')};
+  margin: auto auto 35px auto;
+  font-size: 14px;
+  line-height: 20px;
+`;
