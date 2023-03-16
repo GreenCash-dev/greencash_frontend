0343768986308423c146990c7ad0705c55435972
alpha0212 <qkrcjfgml321@naver.com>
2023-03-16 19:37:25 +0900

    feat(CampaignComponents/Desc): Write a description of the page

---
+++ src/components/CampaignComponents/Desc/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import * as S from './styled';
+
+export const CampaignsDesc: React.FC = () => {
+  return <S.DescText>캠페인을 실천하고 캐시를 얻으세요!</S.DescText>;
+};
---
+++ src/components/CampaignComponents/Desc/styled.ts
@@ -0,0 +1,9 @@
+import styled from '@emotion/styled';
+
+export const DescText = styled.span`
+  font-size: 14px;
+  font-weight: medium;
+
+  text-align: left;
+  color: #868a9a;
+`;
