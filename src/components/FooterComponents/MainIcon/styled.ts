c5a3a3dc364c78b84a016f032e3374794aa9b783
alpha0212 <qkrcjfgml321@naver.com>
2023-03-20 22:12:47 +0900

    refactor(Footer): Merging Components

--- src/components/FooterComponents/MainIcon/styled.ts
+++ src/components/FooterComponents/MainIcon/styled.ts
@@ -1,13 +1,1 @@
 import styled from '@emotion/styled';
-
-export const MainIcon = styled.img`
-  width: 20px;
-  height: 20px;
-  margin-left: 2px;
-`;
-export const MainIconText = styled.div<{ MainTextColor: string }>`
-  color: ${(props) => (props.MainTextColor === '' ? '#acd60a' : '#D9D9D9')};
-  font-size: 13px;
-  font-weight: bold;
-  margin-top: 7px;
-`;
