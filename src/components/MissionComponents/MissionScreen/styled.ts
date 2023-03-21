377653b7c43134f7c22a9a96039d8ca83d7d7b4e
alpha0212 <qkrcjfgml321@naver.com>
2023-03-21 21:28:39 +0900

    MissionScreen styles

---
+++ src/components/MissionComponents/MissionScreen/styled.ts
@@ -0,0 +1,22 @@
+import styled from '@emotion/styled';
+
+export const MissionScreenContainer = styled.div`
+  position: relative;
+  width: 100%;
+  height: 100%;
+  display: flex;
+`;
+
+export const MissionIllustration = styled.img`
+  width: 100%;
+`;
+
+export const TouchHere = styled.div<{ THWidth: string; THHeight: string; marginLeft: string; marginTop: string }>`
+  position: absolute;
+  background: black;
+  width: ${(props) => props.THWidth};
+  height: ${(props) => props.THHeight};
+  margin-left: ${(props) => props.marginLeft};
+  margin-top: ${(props) => props.marginTop};
+  cursor: pointer;
+`;
