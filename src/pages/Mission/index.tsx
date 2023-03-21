52866a2e6129b7e1765d4718caa59411a577c72c
alpha0212 <qkrcjfgml321@naver.com>
2023-03-21 21:30:55 +0900

    feat(pages): Responsive implementation using media queries

--- src/pages/Mission/index.tsx
+++ src/pages/Mission/index.tsx
@@ -1,15 +1,20 @@
-import { MissionHeader } from '@src/components';
+import { MissionHeader, MissionModal, MissionScreenComponent } from '@src/components';
 import React from 'react';
 
 import * as S from './styled';
 
+import MissionIllustration from '@assets/image 24.png';
+import { useMediaQuery } from 'react-responsive';
+
 export const MissionPage: React.FC = () => {
+  const isResponsive = useMediaQuery({ minWidth: '768px' });
+  console.log(isResponsive);
   return (
     <S.MissionContainer>
       <S.MissionHeaderContainer>
         <MissionHeader MissionText="숨은 미션들을 찾고 하나씩 성공해보세요!" />
       </S.MissionHeaderContainer>
-      <S.Footer></S.Footer>
+      <MissionScreenComponent Illustration={MissionIllustration} isReponsive={isResponsive} />
     </S.MissionContainer>
   );
 };
