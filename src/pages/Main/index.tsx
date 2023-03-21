ebbe3a10d2c101238a58f094339763adb2355958
alpha0212 <qkrcjfgml321@naver.com>
2023-03-21 14:35:03 +0900

    Refactor: Footer to App.tsx

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -15,34 +15,19 @@ import {
   Store,
 } from '@src/components';
 import { useSeo } from '@src/hooks';
-import React, { useState } from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import * as S from './styled';
 import { useRecoilState } from 'recoil';
 import { havingCashState } from '@src/atom';
 
-//icons
-import FourSquaresIcon from '@assets/FourSquares.svg';
-import NoneFourSquaresIcon from '@assets/NoneCheck/NoneFourSquares.svg';
-import CheckedBoxIcon from '@assets/CheckedBox.svg';
-import NoneCheckedBoxIcon from '@assets/NoneCheck/NoneCheckedBox.svg';
-import ProfileIcon from '@assets/Profile.svg';
-import NoneProfileIcon from '@assets/NoneCheck/NoneProfile.svg';
-import SearchIcon from '@assets/Search.svg';
-import NoneSearchIcon from '@assets/NoneCheck/NoneSearch.svg';
-
 export const MainPage: React.FC = () => {
   //https://www.crocus.co.kr/1582 css background gradation
-  const location = useLocation();
   const navigate = useNavigate();
-  const [locationPathName, setLocationPath] = useState(location.pathname.split('/')[0]);
   const [havingCash, setHavingCash] = useRecoilState(havingCashState);
   useSeo('메인');
-  const MainObject = { Icon: FourSquaresIcon, NIcon: NoneFourSquaresIcon, Tag: '메인', Path: '' };
-  const MissionObject = { Icon: CheckedBoxIcon, NIcon: NoneCheckedBoxIcon, Tag: '미션', Path: 'mission' };
-  const ProfileObject = { Icon: ProfileIcon, NIcon: NoneProfileIcon, Tag: '프로필', Path: 'profile' };
-  const SearchObject = { Icon: SearchIcon, NIcon: NoneSearchIcon, Tag: '검색', Path: 'search' };
+
   return (
     <>
       <Navbar />
@@ -78,7 +63,6 @@ export const MainPage: React.FC = () => {
           <Question />
         </S.QAContainer>
       </S.Menus>
-      <Footer locationPathName={locationPathName} IconsArr={[MainObject]} />
     </>
   );
 };
