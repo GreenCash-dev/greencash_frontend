41c4b78a1834646a6e9ec59c02581c36aa4d0c4e
alpha0212 <qkrcjfgml321@naver.com>
2023-03-22 20:30:19 +0900

    feat(pages): function close mission modal  send to mission modal

--- src/pages/Mission/index.tsx
+++ src/pages/Mission/index.tsx
@@ -4,17 +4,29 @@ import React from 'react';
 import * as S from './styled';
 
 import MissionIllustration from '@assets/image 24.png';
+import { useSetRecoilState } from 'recoil';
+import { MissionModalState } from '@src/atom/Mission';
 import { useMediaQuery } from 'react-responsive';
 
 export const MissionPage: React.FC = () => {
   const isResponsive = useMediaQuery({ minWidth: '768px' });
-  console.log(isResponsive);
+  const setMissionModalState = useSetRecoilState(MissionModalState);
+  const isOpenOnClick = () => {
+    setMissionModalState((prev) => ({
+      ...prev,
+      view: 'mission',
+    }));
+  };
   return (
     <S.MissionContainer>
       <S.MissionHeaderContainer>
         <MissionHeader MissionText="숨은 미션들을 찾고 하나씩 성공해보세요!" />
       </S.MissionHeaderContainer>
-      <MissionScreenComponent Illustration={MissionIllustration} isReponsive={isResponsive} />
+      <MissionScreenComponent
+        ModalOpenOnClick={isOpenOnClick}
+        Illustration={MissionIllustration}
+        isReponsive={isResponsive}
+      />
     </S.MissionContainer>
   );
 };
