c8fb2b424b3a8223d8fe721a4d59030c0530b5a8
alpha0212 <qkrcjfgml321@naver.com>
2023-03-22 20:28:31 +0900

    feat(modal): create mission modal

--- src/components/Modal/Mission/index.tsx
+++ src/components/Modal/Mission/index.tsx
@@ -1,11 +1,40 @@
-import React from 'react';
+import React, { MouseEventHandler } from 'react';
 
 import * as S from './styled';
+import { CashOnHand } from '@components/MainComponents';
 
-export const MissionModal: React.FC = () => {
+interface MissionModalProps {
+  handleCloseModalOnClick: MouseEventHandler;
+}
+
+export const MissionModal: React.FC<MissionModalProps> = ({ handleCloseModalOnClick }) => {
   return (
     <S.ModalWarp>
-      <S.MissionModalBox></S.MissionModalBox>
+      <S.MissionModalBox>
+        <S.MissionWarpContainer>
+          <S.MissionTopContainer>
+            <S.TitleContainer>
+              <S.MissionTitle>물수건/물걸레 이용하기</S.MissionTitle>
+              <S.MissionDesc>물티슈 대신 물걸레로 청소해요</S.MissionDesc>
+            </S.TitleContainer>
+            <S.YouCanGetCashContainer>
+              <CashOnHand marginTop={'-5.5px'} marginRight="1px" AmountOfCash={1000} />
+            </S.YouCanGetCashContainer>
+          </S.MissionTopContainer>
+          <S.MissionInfoContainer>
+            <S.MissionInfoPoint>물티슈는 환경오염의 주범이에요!</S.MissionInfoPoint>
+            <S.MissionInfoDesc>
+              물티슈 대신 물수건 혹은 물걸레로 청소하고
+              <br /> 캐시를 지급받으세요.
+            </S.MissionInfoDesc>
+          </S.MissionInfoContainer>
+          <S.MissionButtonContainer>
+            <S.MissionButton onClick={handleCloseModalOnClick}>
+              <S.MissionButtonText>미션 수행하기</S.MissionButtonText>
+            </S.MissionButton>
+          </S.MissionButtonContainer>
+        </S.MissionWarpContainer>
+      </S.MissionModalBox>
     </S.ModalWarp>
   );
 };
--- src/components/Modal/Mission/styled.ts
+++ src/components/Modal/Mission/styled.ts
@@ -26,3 +26,81 @@ export const MissionModalBox = styled.div`
   margin: auto auto 0 auto;
   z-index: 100000;
 `;
+
+export const MissionWarpContainer = styled.div`
+  width: 90%;
+  display: flex;
+  flex-direction: column;
+
+  margin: auto auto 0 auto;
+`;
+
+export const MissionTopContainer = styled.div`
+  display: flex;
+`;
+
+export const TitleContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 20px;
+`;
+
+export const MissionTitle = styled.strong`
+  font-size: 22px;
+  font-weight: 600;
+  color: #000000;
+  text-align: left;
+`;
+
+export const MissionDesc = styled.div`
+  font-size: 18px;
+  font-weight: 600;
+  color: #d9d9d9;
+  text-align: left;
+
+  margin-top: 10px;
+`;
+export const YouCanGetCashContainer = styled.div`
+  margin: 0 0 0 auto;
+`;
+
+//MissionInfoStyle
+export const MissionInfoContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  margin-bottom: 30px;
+`;
+
+export const MissionInfoPoint = styled.strong`
+  font-size: 15px;
+  font-weight: 600;
+  color: #000000;
+`;
+export const MissionInfoDesc = styled.span`
+  font-size: 15px;
+  font-weight: 600;
+  color: #aaaaaa;
+`;
+
+//mission Button
+export const MissionButtonContainer = styled.div`
+  width: 100%;
+  margin: auto auto 20px auto;
+`;
+export const MissionButton = styled.button`
+  width: 100%;
+  height: 50px;
+  background: #9ed964;
+  border: none;
+  border-radius: 10px;
+
+  cursor: pointer;
+`;
+
+export const MissionButtonText = styled.strong`
+  font-size: 18px;
+  font-weight: bold;
+  color: #ffffff;
+  text-align: center;
+`;
