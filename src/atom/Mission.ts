d8270d68c38028014dfd38fedd5573268fbe42d2
alpha0212 <qkrcjfgml321@naver.com>
2023-03-23 20:32:46 +0900

    feat(atom): Data to be used for mission modal

--- src/atom/Mission.ts
+++ src/atom/Mission.ts
@@ -16,3 +16,25 @@ export const MissionModalState = atom<MissionModalState>({
   key: 'MissionModalState',
   default: defaultModalState,
 });
+
+//data send to Modal
+export interface MissionDataState {
+  ModalTitle: string;
+  ModalTitleDesc: string;
+  ModalGetCash: number;
+  ModalInfoTitle: string;
+  ModalInfoDesc: string;
+}
+
+const DataState: MissionDataState = {
+  ModalTitle: '',
+  ModalTitleDesc: '',
+  ModalGetCash: 0,
+  ModalInfoTitle: '',
+  ModalInfoDesc: '',
+};
+
+export const missionDataState = atom<MissionDataState>({
+  key: 'missionData',
+  default: DataState,
+});
