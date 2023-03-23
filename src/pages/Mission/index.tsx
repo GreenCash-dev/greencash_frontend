6a85caa071c95af02aa7c01bf9ceaf82f0301862
alpha0212 <qkrcjfgml321@naver.com>
2023-03-23 20:37:20 +0900

    feat(pages): Get data with  click and send it globally management

--- src/pages/Mission/index.tsx
+++ src/pages/Mission/index.tsx
@@ -5,23 +5,22 @@ import * as S from './styled';
 
 import MissionIllustration from '@assets/image 24.png';
 import { useSetRecoilState } from 'recoil';
-import { MissionModalState } from '@src/atom/Mission';
+import { MissionModalState, missionDataState } from '@src/atom/Mission';
 import { useMediaQuery } from 'react-responsive';
 
 export const MissionPage: React.FC = () => {
   const isResponsive = useMediaQuery({ minWidth: '768px' });
-  const [modalInfo, setModalInfo] = useState([
-    {
-      ModalTitle: '',
-      ModalTitleDesc: '',
-      ModalGetCash: 0,
-      ModalInfoTitle: '',
-      ModalInfoDesc: '',
-    },
-  ]);
-  console.log(modalInfo);
   const setMissionModalState = useSetRecoilState(MissionModalState);
-  const isOpenOnClick = () => {
+  const setMissionModalDate = useSetRecoilState(missionDataState);
+  const isOpenOnClick = (a: string, b: string, c: number, d: string, e: string) => {
+    setMissionModalDate((prev) => ({
+      ...prev,
+      ModalTitle: a,
+      ModalTitleDesc: b,
+      ModalGetCash: c,
+      ModalInfoTitle: d,
+      ModalInfoDesc: e,
+    }));
     setMissionModalState((prev) => ({
       ...prev,
       view: 'mission',
@@ -33,7 +32,6 @@ export const MissionPage: React.FC = () => {
         <MissionHeader MissionText="숨은 미션들을 찾고 하나씩 성공해보세요!" />
       </S.MissionHeaderContainer>
       <MissionScreenComponent
-        setModalInfo={setModalInfo}
         ModalOpenOnClick={isOpenOnClick}
         Illustration={MissionIllustration}
         isReponsive={isResponsive}
