6d39861841a96e173cc3bc12ccda9acc5eb03229
alpha0212 <qkrcjfgml321@naver.com>
2023-04-25 00:50:19 +0900

    feat(pages): dev Step Certification page

---
+++ src/pages/StepCertification/index.tsx
@@ -0,0 +1,19 @@
+import { GobackIcon } from '@src/components';
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import * as S from './styled';
+
+export const StepCertificationPage: React.FC = () => {
+  const navigate = useNavigate();
+  return (
+    <S.StepCertification>
+      <S.StepCertificationScreen>
+        <S.GoBackContainer>
+          <GobackIcon MoveWhere={-1} NavigateFunc={navigate} />
+          <S.StepCertificationTitle>단계별 인증</S.StepCertificationTitle>
+        </S.GoBackContainer>
+        <S.StepTitleContainer>asd</S.StepTitleContainer>
+      </S.StepCertificationScreen>
+    </S.StepCertification>
+  );
+};
---
+++ src/pages/StepCertification/styled.ts
@@ -0,0 +1,42 @@
+import styled from '@emotion/styled';
+
+export const StepCertification = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+
+  background-color: #fff;
+`;
+
+export const StepCertificationScreen = styled.div`
+  width: 90%;
+  height: 100vh;
+
+  display: flex;
+  flex-direction: column;
+
+  margin: auto;
+  background: #fff;
+`;
+
+export const GoBackContainer = styled.div`
+  display: flex;
+  margin-top: 40px;
+`;
+
+export const StepCertificationTitle = styled.strong`
+  color: #000;
+
+  font-size: 18px;
+  font-weight: bold;
+
+  margin-top: -3px;
+  margin-left: 15px;
+`;
+
+export const StepTitleContainer = styled.div`
+  width: 100%;
+  display: flex;
+
+  margin-top: 30px;
+`;
