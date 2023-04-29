59dce3cf23eaa58ae6c58b88d0c71975d0249787
alpha0212 <qkrcjfgml321@naver.com>
2023-04-29 15:29:16 +0900

    feat(pages): Add StepTitlePoint

--- src/pages/StepCertification/index.tsx
+++ src/pages/StepCertification/index.tsx
@@ -1,4 +1,5 @@
 import { GobackIcon } from '@src/components';
+import { StepTitlePoint } from '@src/components/StepCertificationComponent';
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import * as S from './styled';
@@ -12,6 +13,7 @@ export const StepCertificationPage: React.FC = () => {
           <GobackIcon MoveWhere={-1} NavigateFunc={navigate} />
           <S.StepCertificationTitle>단계별 인증</S.StepCertificationTitle>
         </S.GoBackContainer>
+        <StepTitlePoint StepNumber={1} />
         <S.StepTitleContainer>asd</S.StepTitleContainer>
       </S.StepCertificationScreen>
     </S.StepCertification>
