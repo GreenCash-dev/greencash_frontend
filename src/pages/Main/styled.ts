fb69810e64f50ab1f6ed14a0e6dd7f014bfaf069
alpha0212 <qkrcjfgml321@naver.com>
2023-04-29 23:30:33 +0900

    design(pages): MainContainer styling

--- src/pages/Main/styled.ts
+++ src/pages/Main/styled.ts
@@ -1,5 +1,10 @@
 import styled from '@emotion/styled';
 
+export const MainContainer = styled.section`
+  width: 100%;
+  height: 100vh;
+  background: rgba(245, 245, 245, 1);
+`;
 export const Menus = styled.section`
   width: 88%;
   margin: auto;
