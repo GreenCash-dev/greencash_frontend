c3dc27f9f96176916cbcfa01b0f4a60fb8245da7
alpha0212 <qkrcjfgml321@naver.com>
2023-05-09 09:06:32 +0900

    feat: create stepData

---
+++ src/data/stepOne.ts
@@ -0,0 +1,88 @@
+interface datasTypes {
+  id: number;
+  text: string;
+  name: string;
+  backgroundColor: string;
+  textColor: string;
+}
+const datas: datasTypes[] = [
+  {
+    id: 1,
+    text: '종이류',
+    name: 'one',
+    backgroundColor: '#141B3D',
+    textColor: '#fff',
+  },
+  {
+    id: 2,
+    text: '종이팩류',
+    name: 'two',
+    backgroundColor: '#141B3D',
+    textColor: '#fff',
+  },
+  {
+    id: 3,
+    text: '유리병류',
+    name: 'three',
+    backgroundColor: '#141B3D',
+    textColor: '#fff',
+  },
+  {
+    id: 4,
+    text: '캔류',
+    name: 'four',
+    backgroundColor: '#141B3D',
+    textColor: '#fff',
+  },
+  {
+    id: 5,
+    text: '의류',
+    name: 'five',
+    backgroundColor: '#141B3D',
+    textColor: '#fff',
+  },
+  {
+    id: 6,
+    text: '플라스틱류',
+    name: 'six',
+    backgroundColor: '#141B3D',
+    textColor: '#fff',
+  },
+  {
+    id: 7,
+    text: 'BOX류',
+    name: 'seven',
+    backgroundColor: '#141B3D',
+    textColor: '#fff',
+  },
+  {
+    id: 8,
+    text: '비닐류',
+    name: 'eight',
+    backgroundColor: '#141B3D',
+    textColor: '#fff',
+  },
+  {
+    id: 9,
+    text: '고철류',
+    name: 'nine',
+    backgroundColor: '#141B3D',
+    textColor: '#fff',
+  },
+  {
+    id: 10,
+    text: '스티로폼',
+    name: 'ten',
+    backgroundColor: '#141B3D',
+    textColor: '#fff',
+  },
+  {
+    id: 11,
+    text: '폐형광등, 폐건전지',
+    name: 'eleven',
+    backgroundColor: '#141B3D',
+    textColor: '#fff',
+  },
+];
+
+export default datas;
