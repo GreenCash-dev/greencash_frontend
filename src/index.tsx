d730dd7ebd4f6c991e88558c1c9cd23a2c41a2fd
alpha0212 <qkrcjfgml321@naver.com>
2023-05-09 09:06:12 +0900

    refactor: remove reportWebcitals.ts

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 import { globalStyle } from './styles';
 import { RecoilRoot } from 'recoil';
@@ -19,8 +18,3 @@ root.render(
     </BrowserRouter>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
