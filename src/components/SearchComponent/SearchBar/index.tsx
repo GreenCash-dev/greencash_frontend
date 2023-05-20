693d6927c0aca5db5e7e7b67427f5584cf5f329e
alpha0212 <qkrcjfgml321@naver.com>
2023-05-20 09:46:16 +0900

    feat: Add searchbar component

---
+++ src/components/SearchComponent/SearchBar/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import * as S from './styled';
+
+import SearchIcon from '@assets/Search.svg';
+
+export const SearchBar: React.FC = () => {
+  return (
+    <S.SearchBarContainer>
+      <S.SearchBarIcon src={SearchIcon} />
+      <S.SearchForm>
+        <S.SearchBarInput placeholder="검색어를 입력해주세요." />
+      </S.SearchForm>
+    </S.SearchBarContainer>
+  );
+};
