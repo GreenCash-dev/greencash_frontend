ff985b2c9c1ef51673dd1a5a0db845c265b65ec5
alpha0212 <qkrcjfgml321@naver.com>
2023-05-20 09:42:49 +0900

    feat: Recent search list

---
+++ src/components/SearchComponent/RecentSearch/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import * as S from './styled';
+
+export const RecentSearch: React.FC = () => {
+  return (
+    <S.RecentSearchContainer>
+      <S.RecentSearchTitle>최근 검색어</S.RecentSearchTitle>
+    </S.RecentSearchContainer>
+  );
+};
