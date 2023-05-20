a92384e629c9ebeef328dd7acffbfb658d8aa370
alpha0212 <qkrcjfgml321@naver.com>
2023-05-20 09:43:15 +0900

    design: Recent search list font styling

---
+++ src/components/SearchComponent/RecentSearch/styled.ts
@@ -0,0 +1,18 @@
+import styled from '@emotion/styled';
+
+export const RecentSearchContainer = styled.div`
+  width: 95%;
+
+  display: flex;
+  flex-direction: column;
+
+  margin-left: 20px;
+`;
+export const RecentSearchTitle = styled.strong`
+  font-size: 16px;
+  font-weight: 500;
+
+  color: #000;
+
+  margin-top: 20px;
+`;
