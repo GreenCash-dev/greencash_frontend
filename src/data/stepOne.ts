ea0d03810930a0a3af224b1ca565dde6e25a79cc
alpha0212 <qkrcjfgml321@naver.com>
2023-05-20 09:48:26 +0900

    feat: Data designate type of api

--- src/data/stepOne.ts
+++ src/data/stepOne.ts
@@ -4,6 +4,13 @@ interface datasTypes {
   name: string;
   backgroundColor: string;
   textColor: string;
+  donotItem: string;
+  otherTextureTitle: string;
+  otherTexture: string;
+  notTextureTitle: string;
+  notTexture: string;
+  doway: string;
+  dowayTip: string;
 }
 const datas: datasTypes[] = [
   {
@@ -12,6 +19,14 @@ const datas: datasTypes[] = [
     name: 'one',
     backgroundColor: '#141B3D',
     textColor: '#fff',
+    donotItem: '종이류로 배출하면 안되는 품목',
+    otherTextureTitle: '*다른 재질과 혼합 구성된 종이: ',
+    otherTexture: '영수증, 금/은박지, 천연재료 벽지, PVC코팅 벽지 등',
+    notTextureTitle: '*종이가 아닌 소재: ',
+    notTexture: '부직포, 플라스틱 합성지',
+    doway: '종이팩은 종이팩 전용수거함에 배출',
+    dowayTip:
+      '(종이팩 전용수거함이 없는 경우에는 종이류와 구분할 수 있도록 가급적 끈 등으로 묶어 종이류 수거함에 배출)',
   },
   {
     id: 2,
@@ -19,6 +34,14 @@ const datas: datasTypes[] = [
     name: 'two',
     backgroundColor: '#141B3D',
     textColor: '#fff',
+    donotItem: '종이팩류로 배출하면 안되는 품목',
+    otherTextureTitle: '*다른 재질과 혼합 구성된 종이: ',
+    otherTexture: '영수증, 금/은박지, 천연재료 벽지, PVC코팅 벽지 등',
+    notTextureTitle: '*종이가 아닌 소재: ',
+    notTexture: '부직포, 플라스틱 합성지',
+    doway: '종이팩은 종이팩 전용수거함에 배출',
+    dowayTip:
+      '(종이팩 전용수거함이 없는 경우에는 종이류와 구분할 수 있도록 가급적 끈 등으로 묶어 종이류 수거함에 배출)',
   },
   {
     id: 3,
@@ -26,6 +49,14 @@ const datas: datasTypes[] = [
     name: 'three',
     backgroundColor: '#141B3D',
     textColor: '#fff',
+    donotItem: '종이류로 배출하면 안되는 품목',
+    otherTextureTitle: '*다른 재질과 혼합 구성된 종이: ',
+    otherTexture: '영수증, 금/은박지, 천연재료 벽지, PVC코팅 벽지 등',
+    notTextureTitle: '*종이가 아닌 소재: ',
+    notTexture: '부직포, 플라스틱 합성지',
+    doway: '종이팩은 종이팩 전용수거함에 배출',
+    dowayTip:
+      '(종이팩 전용수거함이 없는 경우에는 종이류와 구분할 수 있도록 가급적 끈 등으로 묶어 종이류 수거함에 배출)',
   },
   {
     id: 4,
@@ -33,6 +64,14 @@ const datas: datasTypes[] = [
     name: 'four',
     backgroundColor: '#141B3D',
     textColor: '#fff',
+    donotItem: '종이류로 배출하면 안되는 품목',
+    otherTextureTitle: '*다른 재질과 혼합 구성된 종이: ',
+    otherTexture: '영수증, 금/은박지, 천연재료 벽지, PVC코팅 벽지 등',
+    notTextureTitle: '*종이가 아닌 소재: ',
+    notTexture: '부직포, 플라스틱 합성지',
+    doway: '종이팩은 종이팩 전용수거함에 배출',
+    dowayTip:
+      '(종이팩 전용수거함이 없는 경우에는 종이류와 구분할 수 있도록 가급적 끈 등으로 묶어 종이류 수거함에 배출)',
   },
   {
     id: 5,
@@ -40,6 +79,14 @@ const datas: datasTypes[] = [
     name: 'five',
     backgroundColor: '#141B3D',
     textColor: '#fff',
+    donotItem: '종이류로 배출하면 안되는 품목',
+    otherTextureTitle: '*다른 재질과 혼합 구성된 종이: ',
+    otherTexture: '영수증, 금/은박지, 천연재료 벽지, PVC코팅 벽지 등',
+    notTextureTitle: '*종이가 아닌 소재: ',
+    notTexture: '부직포, 플라스틱 합성지',
+    doway: '종이팩은 종이팩 전용수거함에 배출',
+    dowayTip:
+      '(종이팩 전용수거함이 없는 경우에는 종이류와 구분할 수 있도록 가급적 끈 등으로 묶어 종이류 수거함에 배출)',
   },
   {
     id: 6,
@@ -47,6 +94,14 @@ const datas: datasTypes[] = [
     name: 'six',
     backgroundColor: '#141B3D',
     textColor: '#fff',
+    donotItem: '종이류로 배출하면 안되는 품목',
+    otherTextureTitle: '*다른 재질과 혼합 구성된 종이: ',
+    otherTexture: '영수증, 금/은박지, 천연재료 벽지, PVC코팅 벽지 등',
+    notTextureTitle: '*종이가 아닌 소재: ',
+    notTexture: '부직포, 플라스틱 합성지',
+    doway: '종이팩은 종이팩 전용수거함에 배출',
+    dowayTip:
+      '(종이팩 전용수거함이 없는 경우에는 종이류와 구분할 수 있도록 가급적 끈 등으로 묶어 종이류 수거함에 배출)',
   },
   {
     id: 7,
@@ -54,6 +109,14 @@ const datas: datasTypes[] = [
     name: 'seven',
     backgroundColor: '#141B3D',
     textColor: '#fff',
+    donotItem: '종이류로 배출하면 안되는 품목',
+    otherTextureTitle: '*다른 재질과 혼합 구성된 종이: ',
+    otherTexture: '영수증, 금/은박지, 천연재료 벽지, PVC코팅 벽지 등',
+    notTextureTitle: '*종이가 아닌 소재: ',
+    notTexture: '부직포, 플라스틱 합성지',
+    doway: '종이팩은 종이팩 전용수거함에 배출',
+    dowayTip:
+      '(종이팩 전용수거함이 없는 경우에는 종이류와 구분할 수 있도록 가급적 끈 등으로 묶어 종이류 수거함에 배출)',
   },
   {
     id: 8,
@@ -61,6 +124,14 @@ const datas: datasTypes[] = [
     name: 'eight',
     backgroundColor: '#141B3D',
     textColor: '#fff',
+    donotItem: '종이류로 배출하면 안되는 품목',
+    otherTextureTitle: '*다른 재질과 혼합 구성된 종이: ',
+    otherTexture: '영수증, 금/은박지, 천연재료 벽지, PVC코팅 벽지 등',
+    notTextureTitle: '*종이가 아닌 소재: ',
+    notTexture: '부직포, 플라스틱 합성지',
+    doway: '종이팩은 종이팩 전용수거함에 배출',
+    dowayTip:
+      '(종이팩 전용수거함이 없는 경우에는 종이류와 구분할 수 있도록 가급적 끈 등으로 묶어 종이류 수거함에 배출)',
   },
   {
     id: 9,
@@ -68,6 +139,14 @@ const datas: datasTypes[] = [
     name: 'nine',
     backgroundColor: '#141B3D',
     textColor: '#fff',
+    donotItem: '종이류로 배출하면 안되는 품목',
+    otherTextureTitle: '*다른 재질과 혼합 구성된 종이: ',
+    otherTexture: '영수증, 금/은박지, 천연재료 벽지, PVC코팅 벽지 등',
+    notTextureTitle: '*종이가 아닌 소재: ',
+    notTexture: '부직포, 플라스틱 합성지',
+    doway: '종이팩은 종이팩 전용수거함에 배출',
+    dowayTip:
+      '(종이팩 전용수거함이 없는 경우에는 종이류와 구분할 수 있도록 가급적 끈 등으로 묶어 종이류 수거함에 배출)',
   },
   {
     id: 10,
@@ -75,6 +154,14 @@ const datas: datasTypes[] = [
     name: 'ten',
     backgroundColor: '#141B3D',
     textColor: '#fff',
+    donotItem: '종이류로 배출하면 안되는 품목',
+    otherTextureTitle: '*다른 재질과 혼합 구성된 종이: ',
+    otherTexture: '영수증, 금/은박지, 천연재료 벽지, PVC코팅 벽지 등',
+    notTextureTitle: '*종이가 아닌 소재: ',
+    notTexture: '부직포, 플라스틱 합성지',
+    doway: '종이팩은 종이팩 전용수거함에 배출',
+    dowayTip:
+      '(종이팩 전용수거함이 없는 경우에는 종이류와 구분할 수 있도록 가급적 끈 등으로 묶어 종이류 수거함에 배출)',
   },
   {
     id: 11,
@@ -82,6 +169,14 @@ const datas: datasTypes[] = [
     name: 'eleven',
     backgroundColor: '#141B3D',
     textColor: '#fff',
+    donotItem: '종이류로 배출하면 안되는 품목',
+    otherTextureTitle: '*다른 재질과 혼합 구성된 종이: ',
+    otherTexture: '영수증, 금/은박지, 천연재료 벽지, PVC코팅 벽지 등',
+    notTextureTitle: '*종이가 아닌 소재: ',
+    notTexture: '부직포, 플라스틱 합성지',
+    doway: '종이팩은 종이팩 전용수거함에 배출',
+    dowayTip:
+      '(종이팩 전용수거함이 없는 경우에는 종이류와 구분할 수 있도록 가급적 끈 등으로 묶어 종이류 수거함에 배출)',
   },
 ];
 
