7e9a6b1fea003123e678bed879f8fdf6f88ecb39
alpha0212 <qkrcjfgml321@naver.com>
2023-05-25 23:34:37 +0900

    design: Search page init styling

---
+++ src/pages/Search/styled.ts
@@ -0,0 +1,9 @@
+import styled from '@emotion/styled';
+
+export const SearchContainer = styled.section`
+  width: 100%;
+  height: 100vh;
+  margin: auto;
+  display: flex;
+  flex-direction: column;
+`;
