3b7d8b2e5471b3bf4c0005f5a1cab91811a9b5de
alpha0212 <qkrcjfgml321@naver.com>
2023-05-25 23:33:31 +0900

    feat: Now step level

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -15,7 +15,7 @@ import {
   Store,
 } from '@src/components';
 import { useSeo } from '@src/hooks';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import * as S from './styled';
@@ -26,8 +26,19 @@ export const MainPage: React.FC = () => {
   //https://www.crocus.co.kr/1582 css background gradation
   const navigate = useNavigate();
   const [havingCash, setHavingCash] = useRecoilState(havingCashState);
+  const [stepOne, setStepOne] = useState<boolean>(false);
+  const [stepTwo, setStepTwo] = useState<boolean>(false);
+  const [stepThree, setStepThree] = useState<boolean>(false);
+  const [stepFour, setStepFour] = useState<boolean>(false);
+  const [level, setLevel] = useState<string>('');
   useSeo('메인');
-
+  useEffect(() => {
+    setLevel(localStorage.getItem('stepLevel'));
+    if (level === '1') {
+      setStepOne(true);
+    }
+  }, []);
+  console.log(stepOne);
   return (
     <S.MainContainer>
       <Navbar />
@@ -40,8 +51,8 @@ export const MainPage: React.FC = () => {
         <S.OnecCertificationContainer onClick={() => navigate('/once')}>
           <OnceCertification />
         </S.OnecCertificationContainer>
-        <S.StepCertificationContainer onClick={() => navigate('/step')}>
-          <StepCertification StepOne={false} StepTwo={false} StepThree={false} StepFour={false} />
+        <S.StepCertificationContainer onClick={() => navigate('/step/one')}>
+          <StepCertification StepOne={stepOne} StepTwo={stepTwo} StepThree={stepThree} StepFour={stepFour} />
         </S.StepCertificationContainer>
         <S.GuideLineContainer>
           <GuideLine />
