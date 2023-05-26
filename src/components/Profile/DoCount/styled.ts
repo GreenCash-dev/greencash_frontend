f438e5fa4a809a39cd72b216e94c866d534f5d9f
alpha0212 <qkrcjfgml321@naver.com>
2023-05-26 16:02:27 +0900

    design: DoCount initailzation styling

---
+++ src/components/Profile/DoCount/styled.ts
@@ -0,0 +1,25 @@
+import styled from '@emotion/styled';
+
+export const DoCountContainer = styled.div`
+  width: 100%;
+  height: 45px;
+
+  display: flex;
+`;
+
+export const CountInfoContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  margin: auto;
+`;
+export const CountNumber = styled.strong``;
+export const CountTitle = styled.span``;
+
+export const CenterDiv = styled.div`
+  width: 2.5px;
+  height: 100%;
+
+  border-radius: 15px;
+  background-color: rgba(160, 160, 160, 1);
+`;
