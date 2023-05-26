c307ba26c27c433552a3be523a489a6d6d98b7a5
alpha0212 <qkrcjfgml321@naver.com>
2023-05-26 16:03:35 +0900

    feat: Add component on ProfilePage

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 
 import * as S from './styled';
-import { Navbar, ProfileInfo } from '@src/components';
+import { DoCount, MissionProgress, Navbar, ProfileInfo } from '@src/components';
 
 export const ProfilePage: React.FC = () => {
   return (
     <S.ProfileContainer>
       <Navbar />
       <ProfileInfo />
+      <MissionProgress />
+      <DoCount />
     </S.ProfileContainer>
   );
 };
