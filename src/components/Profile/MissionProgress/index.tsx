b069c842304f37f5555eafa93028f1b533627e0c
alpha0212 <qkrcjfgml321@naver.com>
2023-05-26 16:02:48 +0900

    feat: Add MissionProgress component

---
+++ src/components/Profile/MissionProgress/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import * as S from './styled';
+
+import CheckBoxPNG from '@assets/checkbox_4.png';
+
+export const MissionProgress: React.FC = () => {
+  return (
+    <S.MissionProgessContainer>
+      <S.MissionProgressTop>
+        <S.MissionProgressCheckbox src={CheckBoxPNG} />
+        <S.MissionProgressTitle>오늘 하루 미션 수행 정도</S.MissionProgressTitle>
+      </S.MissionProgressTop>
+      <S.MissionProgressBottom>
+        <S.MissionProgress value={30} max={100}></S.MissionProgress>
+        <S.MissionProgressPercent>{`${30}%`}</S.MissionProgressPercent>
+      </S.MissionProgressBottom>
+    </S.MissionProgessContainer>
+  );
+};
