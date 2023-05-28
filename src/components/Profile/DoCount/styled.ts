5600df84f05fa2f2f91f84957e7659ce53f53843
alpha0212 <qkrcjfgml321@naver.com>
2023-05-28 17:26:20 +0900

    design: Complete profile docount styling

--- src/components/Profile/DoCount/styled.ts
+++ src/components/Profile/DoCount/styled.ts
@@ -5,6 +5,8 @@ export const DoCountContainer = styled.div`
   height: 45px;
 
   display: flex;
+
+  margin-top: 36px;
 `;
 
 export const CountInfoContainer = styled.div`
@@ -13,8 +15,17 @@ export const CountInfoContainer = styled.div`
 
   margin: auto;
 `;
-export const CountNumber = styled.strong``;
-export const CountTitle = styled.span``;
+export const CountNumber = styled.strong`
+  font-size: 16px;
+  font-weight: 500;
+  text-align: center;
+`;
+export const CountTitle = styled.span`
+  font-size: 12px;
+  font-weight: 400;
+
+  margin-top: 8px;
+`;
 
 export const CenterDiv = styled.div`
   width: 2.5px;
