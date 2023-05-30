8cd245e73d7b762d5dc21dc84ef98834586d5134
alpha0212 <qkrcjfgml321@naver.com>
2023-05-30 22:25:57 +0900

    feat: Firebase Inital setting

---
+++ src/firebase/clientApp.ts
@@ -0,0 +1,22 @@
+import { initializeApp, getApp, getApps } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+};
+
+//클라이언트에서 랜더링 하지 않게 SSR을 설정해줌
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const firestore = getFirestore(app);
+const auth = getAuth(app);
+const storage = getStorage(app);
+export const db = getFirestore(app);
+
+export { app, auth, firestore, storage };
