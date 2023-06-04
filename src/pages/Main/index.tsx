84370dec11f675dc0822a58b14b2078e38d3294f
alpha0212 <qkrcjfgml321@naver.com>
2023-06-04 19:55:00 +0900

    refactor: rendering developing

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,18 +1,11 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { Suspense, useCallback, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil';
 import { collection, getDocs } from 'firebase/firestore';
-import { auth, db } from '@src/firebase/clientApp';
-import { useAuthState } from 'react-firebase-hooks/auth';
+import { db } from '@src/firebase/clientApp';
 
 import {
   Campaign,
   CashOnHand,
-  Footer,
-  FooterMainIcon,
-  FooterMissionIcon,
-  FooterProfileIcon,
-  FooterSearchIcon,
   Give,
   GuideLine,
   Navbar,
@@ -25,6 +18,7 @@ import {
 import * as S from './styled';
 import { useSeo } from '@src/hooks';
 import { havingCashState } from '@src/atom';
+import { SearchPage } from '../Search';
 
 interface UserInfo {
   cash: number;
@@ -37,18 +31,15 @@ interface cashType {
 
 export const MainPage: React.FC = () => {
   const navigate = useNavigate();
-  const havingCash = useRecoilValue(havingCashState);
   const [stepOne, setStepOne] = useState<boolean>(false);
   const [stepTwo, setStepTwo] = useState<boolean>(false);
   const [stepThree, setStepThree] = useState<boolean>(false);
   const [stepFour, setStepFour] = useState<boolean>(false);
   const [level, setLevel] = useState<string>('');
 
-  const setHavingCashState = useSetRecoilState(havingCashState);
-
   useSeo('메인');
 
-  const [cash, setCash] = useState<cashType[]>([]);
+  const [cash, setCash] = useState<cashType>();
   const [userInfo, setUserInfo] = useState<UserInfo[]>([
     {
       cash: 0,
@@ -64,66 +55,66 @@ export const MainPage: React.FC = () => {
    *  cash: doc.data().cash,
       username: doc.data().username,
    */
+  let GreenCashCalculate = 0;
+  const [havingCash, setHavingCash] = useState<number>(0);
   const getCashs = useCallback(async () => {
     const data = await getDocs(greencashCollectionRef);
     const GreenCashData = data.docs.map((doc) => ({
       cash: doc.data().cash,
       username: doc.data().username,
     }));
-    const OnlyCash = data.docs.map((doc) => doc.data().cash);
     setUserInfo(GreenCashData);
-    console.log(OnlyCash[0], 'ㅎㅇ');
-    setCash(OnlyCash);
+    userInfo
+      .filter((data) => data.username === username)
+      .map((data2) => {
+        GreenCashCalculate += data2.cash;
+        setHavingCash(GreenCashCalculate);
+        console.log(havingCash, 1);
+      });
   }, []);
-  const filterData = useCallback(() => {
-    console.log(0);
-  }, []);
-  // eslint-disable-next-line prefer-const
-  let asd = 0;
   useEffect(() => {
+    console.log(userInfo, '?');
     getCashs();
-    filterData();
     setUserName(localStorage.getItem('Authentication'));
-    cash.forEach((v) => {
-      asd += v.cash;
-    });
-  }, []);
-  console.log(userInfo, cash);
+  }, [GreenCashCalculate]);
+  console.log(havingCash);
   return (
-    <S.MainContainer>
-      <Navbar />
-      <S.Menus>
-        <S.CashOnHandContainer>
-          <S.CashOnHandPosition>
-            <CashOnHand marginTop="-2.5px" marginRight="1px" AmountOfCash={asd} />
-          </S.CashOnHandPosition>
-        </S.CashOnHandContainer>
-        <S.OnecCertificationContainer onClick={() => navigate('/once')}>
-          <OnceCertification />
-        </S.OnecCertificationContainer>
-        <S.StepCertificationContainer onClick={() => navigate('/step/one')}>
-          <StepCertification StepOne={stepOne} StepTwo={stepTwo} StepThree={stepThree} StepFour={stepFour} />
-        </S.StepCertificationContainer>
-        <S.GuideLineContainer>
-          <GuideLine />
-        </S.GuideLineContainer>
-        <S.InteractionContainer>
-          <S.CampaignContainer>
-            <Campaign />
-          </S.CampaignContainer>
-          <S.StoreGiveContainer>
-            <S.StoreContainer>
-              <Store />
-            </S.StoreContainer>
-            <S.DoGiveContainer>
-              <Give />
-            </S.DoGiveContainer>
-          </S.StoreGiveContainer>
-        </S.InteractionContainer>
-        <S.QAContainer>
-          <Question />
-        </S.QAContainer>
-      </S.Menus>
-    </S.MainContainer>
+    <Suspense fallback={<SearchPage />}>
+      <S.MainContainer>
+        <Navbar />
+        <S.Menus>
+          <S.CashOnHandContainer>
+            <S.CashOnHandPosition>
+              <CashOnHand marginTop="-2.5px" marginRight="1px" AmountOfCash={havingCash} />
+            </S.CashOnHandPosition>
+          </S.CashOnHandContainer>
+          <S.OnecCertificationContainer onClick={() => navigate('/once')}>
+            <OnceCertification />
+          </S.OnecCertificationContainer>
+          <S.StepCertificationContainer onClick={() => navigate('/step/one')}>
+            <StepCertification StepOne={stepOne} StepTwo={stepTwo} StepThree={stepThree} StepFour={stepFour} />
+          </S.StepCertificationContainer>
+          <S.GuideLineContainer>
+            <GuideLine />
+          </S.GuideLineContainer>
+          <S.InteractionContainer>
+            <S.CampaignContainer>
+              <Campaign />
+            </S.CampaignContainer>
+            <S.StoreGiveContainer>
+              <S.StoreContainer>
+                <Store />
+              </S.StoreContainer>
+              <S.DoGiveContainer>
+                <Give />
+              </S.DoGiveContainer>
+            </S.StoreGiveContainer>
+          </S.InteractionContainer>
+          <S.QAContainer>
+            <Question />
+          </S.QAContainer>
+        </S.Menus>
+      </S.MainContainer>
+    </Suspense>
   );
 };
