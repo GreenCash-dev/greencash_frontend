9775018d49d7c736a27df3db5d19822ef36e4de7
alpha0212 <qkrcjfgml321@naver.com>
2023-06-09 20:28:18 +0900

    feat: handleChange input value

--- src/components/DoGiveComponent/Input/index.tsx
+++ src/components/DoGiveComponent/Input/index.tsx
@@ -3,16 +3,20 @@ import React, { SetStateAction } from 'react';
 import * as S from './styled';
 
 interface DoGiveCashProps {
-  setGiveCash: React.Dispatch<SetStateAction<string>>;
+  setGiveCash: React.Dispatch<SetStateAction<number>>;
 }
 
 export const DoGiveInput: React.FC<DoGiveCashProps> = ({ setGiveCash }) => {
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = parseInt(event.target.value);
+    setGiveCash(value);
+  };
   return (
     <S.DoGiveInputElement
       pattern="^[0-9]*"
       inputMode="numeric"
       type="number"
-      onChange={(event) => setGiveCash(event.target.value)}
+      onChange={handleChange}
       placeholder="캐시를 입력하세요"
     />
   );
